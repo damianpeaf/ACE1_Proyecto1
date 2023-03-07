483be12af17619d6b200d6cdce5d5f0fed05d2e2
Alvaro Garcia <3046971840116@ingenieria.usac.edu.gt>
2023-03-07 09:34:31 -0600

    feat: login almost

--- test/bluetooth/ide/ide.ino
+++ test/bluetooth/ide/ide.ino
@@ -1,11 +1,16 @@
 String text; 
-
+bool login = false;
 void setup() {
   // put your setup code here, to run once:
   pinMode(13,OUTPUT);
   Serial1.begin(9600);
+  Serial.begin(9600);
+  Serial.println("Conectado");
 }
-
+void sendToken() {
+  delay(4000);
+  Serial1.write("123ABC");
+}
 void loop() {
   // put your main code here, to run repeatedly:
   while(Serial1.available()){
@@ -14,12 +19,15 @@ void loop() {
     text += c;
   }
   if (text.length() > 0){
-    Serial1.println(text);
     if(text == "on"){
       digitalWrite(13,HIGH);
     }
     if(text == "off"){
       digitalWrite(13,LOW);
+    }
+    if(text == "connected"){
+      Serial.println(text);
+      sendToken();
     }
     text="";
   }
