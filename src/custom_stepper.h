da4dc5b5f105306eb7dc9a87f651961a189bb1de
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-03-07 21:56:40 -0600

    feat: custom steppter class

---
+++ src/custom_stepper.h
@@ -0,0 +1,13 @@
+#ifndef STEPPER_H
+#define STEPPER_H
+
+#include <Arduino.h>
+#include <stepper.h>
+
+int stepsPerRevolution = 200; // Número de pasos por revolución
+
+class Stepper
+{
+}
+
+#endif
